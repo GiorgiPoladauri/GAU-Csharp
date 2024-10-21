7ce3074f59807644e8afdd8c93eccf3541557f03
GiorgiPoladauri <165472736+GiorgiPoladauri@users.noreply.github.com>
2024-10-22 02:27:03 +0400

    Create HomeWork1.cs

---
+++ HomeWork1.cs
@@ -0,0 +1,47 @@
+﻿Console.WriteLine("Hello ! Please, tell us your name");
+string Name = Console.ReadLine();
+Console.WriteLine("Please, tell us your surname");
+string Surname = Console.ReadLine();
+Console.WriteLine("Please, tell us your age");
+int Age = int.Parse(Console.ReadLine());
+Console.WriteLine("Please, type your city(Berlin/London/Washington) : ");
+string City = Console.ReadLine();
+string Country="";
+if (City == "Berlin")
+{
+    Country = "Germany";
+}
+else if (City == "London")
+{
+    Country = "England";
+}
+else if (City == "Washington")
+{
+    Country = "USA";
+}
+else
+{
+    Console.WriteLine("Unknown country, try again ! ");
+}
+if (Country == "Germany" || Country == "England")
+{
+    if (Age > 18)
+    {
+        Console.WriteLine("You can buy alcohol");
+    }
+    else
+    {
+        Console.WriteLine("You can not buy alcohol");
+    }
+}
+else if (Country == "USA")
+{
+    if (Age > 21)
+    {
+        Console.WriteLine("You can buy alcohol");
+    }
+    else
+    {
+        Console.WriteLine("You can't buy alcohol, sorry !");
+    }
+}
