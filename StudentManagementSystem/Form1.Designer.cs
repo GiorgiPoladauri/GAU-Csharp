42846cc473124d814cfbd29a50da7656b2eb4e3d
GiorgiPoladauri <Giorgi.poladauri15@yahoo.com>
2024-12-04 00:47:32 +0400

    Initial commit for HomeWork5Proj

---
+++ StudentManagementSystem/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace StudentManagementSystem
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            button1 = new Button();
+            dataGridView1 = new DataGridView();
+            textBox1 = new TextBox();
+            comboBox1 = new ComboBox();
+            toolTip1 = new ToolTip(components);
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(31, 288);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 0;
+            button1.Text = "Add";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(470, 12);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(300, 188);
+            dataGridView1.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(31, 83);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 27);
+            textBox1.TabIndex = 2;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(619, 222);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(151, 28);
+            comboBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(31, 245);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(125, 27);
+            textBox2.TabIndex = 4;
+            toolTip1.SetToolTip(textBox2, "Please, be careful inputing GPA here, GPA example is : 0.7 or 3.9 ");
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(31, 136);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(125, 27);
+            textBox3.TabIndex = 5;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(31, 191);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(125, 27);
+            textBox4.TabIndex = 6;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(28, 12);
+            label1.Name = "label1";
+            label1.Size = new Size(130, 28);
+            label1.TabIndex = 7;
+            label1.Text = "Add Student";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 7F);
+            label2.Location = new Point(28, 222);
+            label2.Name = "label2";
+            label2.Size = new Size(108, 15);
+            label2.TabIndex = 8;
+            label2.Text = "Enter Students GPA";
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 7F);
+            label3.Location = new Point(28, 168);
+            label3.Name = "label3";
+            label3.Size = new Size(89, 15);
+            label3.TabIndex = 9;
+            label3.Text = "Enter Birth Date";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 7F);
+            label4.Location = new Point(28, 113);
+            label4.Name = "label4";
+            label4.Size = new Size(93, 15);
+            label4.TabIndex = 10;
+            label4.Text = "Enter Last Name";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 7F);
+            label5.Location = new Point(28, 60);
+            label5.Name = "label5";
+            label5.Size = new Size(94, 15);
+            label5.TabIndex = 11;
+            label5.Text = "Enter First Name";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(782, 403);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(comboBox1);
+            Controls.Add(textBox1);
+            Controls.Add(dataGridView1);
+            Controls.Add(button1);
+            Cursor = Cursors.Cross;
+            FormBorderStyle = FormBorderStyle.Fixed3D;
+            MaximizeBox = false;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "StudentMT";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private DataGridView dataGridView1;
+        private TextBox textBox1;
+        private ComboBox comboBox1;
+        private ToolTip toolTip1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+    }
+}
---
+++ StudentManagementSystem/Student.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace StudentManagementSystem
+{
+    public class Student
+    {
+        private string FirstName { get; set; }
+        private int ID { get; set; }
+        private string LastName { get; set; }
+        private DateTime BirthDate { get; set; }
+        private float gpa;
+
+        public float GPA
+        {
+            get { return gpa; }
+            set
+            {
+                if (value < 0 || value > 4.3)
+                {
+                    throw new Exception("Yoo, something's wrong with your GPA, CHECK!");
+                }
+                gpa = value;
+            }
+        }
+
+        public int CalculateAge()
+        {
+            int Age = DateTime.Now.Year - BirthDate.Year;
+            if (DateTime.Now < BirthDate.AddYears(Age))
+            {
+                Age--;
+            }
+
+            if (Age < 0)
+            {
+                throw new Exception("Are you from the future?");
+            }
+
+            return Age;
+        }
+
+        public string FullNameOfStudent()
+        {
+            return FirstName + " " + LastName;
+        }
+    }
+}
