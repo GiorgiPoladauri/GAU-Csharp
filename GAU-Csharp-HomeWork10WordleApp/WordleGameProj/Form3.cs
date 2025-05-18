2a1e2f1c4c391dfa4b51152b23edd24d85ec239d
Giorgi Poladauri <Giorgi.Poladauri15@yahoo.com>
2025-05-18 22:04:28 +0400

    Updated And Made It Correctooo !

---
+++ GAU-Csharp-HomeWork10WordleApp/WordleGameProj/Form3.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace WordleGameProj
+{
+    public partial class Form3 : Form
+    {
+        public Form3(string email)
+        {
+            InitializeComponent();
+            textBox1.Text = email;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string email = textBox1.Text;
+            string password = textBox2.Text.Trim();
+
+            if (string.IsNullOrEmpty(password))
+            {
+                MessageBox.Show("Password cannot be empty!");
+                return;
+            }
+
+            if (!checkBox1.Checked)
+            {
+                MessageBox.Show("You must agree to the terms before registering!", "Registration Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            using (var context = new WordleGameDatabaseEntities())
+            {
+                bool userExists = context.Users.Any(u => u.Email == email);
+
+                if (!userExists)
+                {
+                    var newUser = new User
+                    {
+                        Email = email,
+                        Password = password,
+                        IsFirstCheckboxChecked = true
+                    };
+
+                    context.Users.Add(newUser);
+                    context.SaveChanges();
+                }
+            }
+
+            this.Hide();
+            Form4 form4 = new Form4(email);
+            form4.ShowDialog();
+            this.Close();
+        }
+    }
+}
